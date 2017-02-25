14ed87133a6978da9891f8241ba290df3a816306
tapeinosyne <ndr@qef.io>
2017-02-25 23:43:04 +0000

    Test basic functionality

---
+++ tests/lib.rs
@@ -0,0 +1,25 @@
+extern crate atlatl;
+extern crate rand;
+extern crate quickcheck;
+
+use quickcheck::{quickcheck};
+use std::collections::BTreeMap;
+
+use atlatl::*;
+use atlatl::fst::*;
+
+
+#[test]
+fn fst_output_matches_source() {
+    fn property(btree: BTreeMap<Vec<u8>, u16>) -> bool {
+        let b = fst::builder::Builder::from_iter(btree.iter().map(|(k, &v)| (k, v)));
+        let fst = FST::from_builder(&b);
+
+        btree.iter().all(|(k, &from_btree)| {
+            let from_fst = fst.get(k).unwrap();
+            from_fst == from_btree
+        })
+    }
+
+    quickcheck(property as fn(BTreeMap<Vec<u8>, u16>) -> bool);
+}
